fb3017c748556da55f3a01816e0fde419af4924b
NadezdaSolt <nadiyaleonova1995@yandex.ru>
2018-01-25 15:20:45 +0300

    second commit

--- mysite/settings (copy).py
+++ mysite/settings (copy).py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,12 +75,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-	'USER': 'postgres',
-	'PASSWORD': 'qwerty',
-	'HOST': 'localhost',
-	'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
